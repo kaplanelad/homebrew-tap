ccf28c41083f40cd9f6c98fbf81e1e3c291224d0
elad <kaplan.elad@gmail.com>
2021-12-29 20:58:24 +0200

    Update rust-github-actions.rb

--- rust-github-actions.rb
+++ rust-github-actions.rb
@@ -1,5 +1,28 @@
 
-class MyFormula < Formula
-  url "https://github.com/kaplanelad/rust-github-actions/releases/download/v0.2.6/rust-github-actions-v0.2.6-x86_64-macos.tar.xz"
-  sha256 "fe6b421b5dae908c9cc5e3e02c9c7f2297fee18b920b21aafe60f5424e40a01a"
+class rustgithubactions < Formula
+  
+  desc ""
+  homepage "https://github.com/kaplanelad/rust-github-actions"
+  version "0.2.6"
+  license "Apache 2.0"
+  
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/kaplanelad/rust-github-actions/releases/download/v0.2.6/rust-github-actions-v0.2.6-x86_64-macos.tar.xz"
+    sha256 "<OLDSHA>"
+  end
+ 
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/kaplanelad/rust-github-actions/releases/download/v0.2.6/rust-github-actions-v0.2.6-x86_64-linux.tar.xz"
+    sha256 "<OLDSHA>"
+  end
+  
+  if OS.linux? && Hardware::CPU.arm? 
+    url "https://github.com/kaplanelad/rust-github-actions/releases/download/v0.2.6/rust-github-actions-v0.2.6-aarch64-linux.tar.xz"
+    sha256 "<OLDSHA>"
+  end
+  
+  def install
+    bin.install "rustgithubactions"
+  end
+ 
 end
