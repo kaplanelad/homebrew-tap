ea2559f7c145f11943fab480c7f4075dd190619c
elad <kaplan.elad@gmail.com>
2021-12-29 20:34:20 +0200

    updating formula for rust-github-actions

--- rust-github-actions.rb
+++ rust-github-actions.rb
@@ -1,5 +1,5 @@
 
 class MyFormula < Formula
-  url "https://github.com/kaplanelad/rust-github-actions/releases/download/v0.2.4/rust-github-actions-v0.2.4-x86_64-macos.tar.xz"
-  sha256 "<OLDSHA>"
+  url "https://github.com/kaplanelad/rust-github-actions/releases/download/v0.2.5/rust-github-actions-v0.2.5-x86_64-macos.tar.xz"
+  sha256 "4f2feef4ce532d92f55fd75032b50a414af0eb75ae639e4293602602159b5e48"
 end
