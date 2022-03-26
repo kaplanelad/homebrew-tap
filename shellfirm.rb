19234f19dfde8de3d2a2f78a9a0f581a41fc6c16
Elad Kaplan <kaplan.elad@gmail.com>
2022-03-26 20:15:56 +0300

    Update shellfirm.rb

--- shellfirm.rb
+++ shellfirm.rb
@@ -3,22 +3,22 @@ class Shellfirm < Formula
   
   desc "Intercept any risky patterns (default or defined by you) and prompt you a small challenge for double verification"
   homepage "https://github.com/kaplanelad/shellfirm"
-  version "0.1.4"
+  version "0.1.5"
   license "Apache 2.0"
   
   if OS.mac?
-    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.1.4/shellfirm-v0.1.4-x86_64-macos.tar.xz"
-    sha256 "db0fc44caced69b01c8aba3e5028280bc86c09fdf4cc7282bbe466e6207fdf79"
+    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.1.5/shellfirm-v0.1.5-x86_64-macos.tar.xz"
+    sha256 "dc5352f32f26c8c4bc715818abe07482c4b0e2214f9095c3b5dafde7b512bd47"
   end
  
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.1.4/shellfirm-v0.1.4-x86_64-linux.tar.xz"
-    sha256 "62117aee7a93ffd501a96bc1169f1173cba2a5aea602fa60acbef8226a968f7d"
+    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.1.5/shellfirm-v0.1.5-x86_64-linux.tar.xz"
+    sha256 "18bebc63d1bfae63b587f2e2ba374c53ec8328dfc9855ab62013aaed88c9e3ae"
   end
   
   if OS.linux? && Hardware::CPU.arm? 
-    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.1.4/shellfirm-v0.1.4-aarch64-linux.tar.xz"
-    sha256 "ada9a76c97ea3dda8ae75bdff3afbff51dbd17b9dcb3fb1f16b1df9037352f61"
+    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.1.5/shellfirm-v0.1.5-aarch64-linux.tar.xz"
+    sha256 "7d38d9c999716e3cf6622e0418a065dcd92dee4b7457cf290a9e6179cec8d692"
   end
   
   def install
