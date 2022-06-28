e4689ab343dd9a0c41c04474387431bd6211edae
Elad Kaplan <kaplan.elad@gmail.com>
2022-06-28 21:32:05 +0300

    Create shellclear.rb

---
+++ shellclear.rb
@@ -0,0 +1,10 @@
+class Shellclear < Formula
+  desc "CLI tool for mange shell history"
+  homepage "https://github.com/rusty-ferris-club/shellclear"
+  url "-----"
+  version "-----"
+  sha256 "-----"
+  def install
+    bin.install "shellclear"
+  end
+end
