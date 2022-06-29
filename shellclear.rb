a16e22acceba01b0086b96b33de84613e45469c3
Elad Kaplan <kaplan.elad@gmail.com>
2022-06-29 22:11:31 +0300

    Update shellclear.rb

--- shellclear.rb
+++ shellclear.rb
@@ -1,9 +1,9 @@
 class Shellclear < Formula
   desc "Preventive sensetive data in your shell history"
   homepage "https://github.com/rusty-ferris-club/shellclear"
-  url ""
-  version ""
-  sha256 ""
+  url "https://github.com/rusty-ferris-club/shellclear/releases/download/v0.1.0/shellclear-v0.1.0-x86_64-macos.tar.xz"
+  version "0.1.0"
+  sha256 "old"
   
   def install
     bin.install "shellclear"
