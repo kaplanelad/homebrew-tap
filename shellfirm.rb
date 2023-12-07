d3a5acc50fdf2c1aba182e45a84bfece8608dc6a
Elad Kaplan <kaplan.elad@gmail.com>
2023-12-07 14:57:49 +0200

    updating formula for shellfirm

--- shellfirm.rb
+++ shellfirm.rb
@@ -3,8 +3,8 @@ class Shellfirm < Formula
   homepage "https://github.com/kaplanelad/shellfirm"
   license "Apache 2.0"
   if RUBY_PLATFORM.include? "arm64"
-    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.2.7/shellfirm-v0.2.7-aarch64-macos.tar.xz"
-    sha256 "e5cabf02e572c63c44494d40ea0596cb9890c2e7e3181c61f5c715e23d1e9d1a"
+    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.2.9/shellfirm-v0.2.9-x86_64-macos.tar.xz"
+    sha256 "cfafefa1b80f17a5acf2e3b2e0f15f9836903fa9f8a2f76f1ed0a8e5e176ee7b"
   else
     url "https://github.com/kaplanelad/shellfirm/releases/download/v0.2.7/shellfirm-v0.2.7-x86_64-macos.tar.xz"
     sha256 "ba65e47b93b363bf1921897cc81fe32cd7a4bd7feaeca0487f894c7e46d2eb29"
