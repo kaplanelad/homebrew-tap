1a57345ec91b5532351aed052942e3ca1777799b
Elad Kaplan <kaplan.elad@gmail.com>
2023-12-07 15:12:15 +0200

    Update shellfirm.rb

--- shellfirm.rb
+++ shellfirm.rb
@@ -2,13 +2,8 @@ class Shellfirm < Formula
   desc "Intercept any risky patterns (default or defined by you) and prompt you a small challenge for double verification"
   homepage "https://github.com/kaplanelad/shellfirm"
   license "Apache 2.0"
-  if RUBY_PLATFORM.include? "arm64"
-    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.2.9/shellfirm-v0.2.9-x86_64-macos.tar.xz"
-    sha256 "cfafefa1b80f17a5acf2e3b2e0f15f9836903fa9f8a2f76f1ed0a8e5e176ee7b"
-  else
-    url "https://github.com/kaplanelad/shellfirm/releases/download/v0.2.9/shellfirm-v0.2.9-x86_64-macos.tar.xz"
-    sha256 "cfafefa1b80f17a5acf2e3b2e0f15f9836903fa9f8a2f76f1ed0a8e5e176ee7b"
-  end
+  url "https://github.com/kaplanelad/shellfirm/releases/download/v0.2.9/shellfirm-v0.2.9-x86_64-macos.tar.xz"
+  sha256 "cfafefa1b80f17a5acf2e3b2e0f15f9836903fa9f8a2f76f1ed0a8e5e176ee7b"
 
   def install
     bin.install "shellfirm"
