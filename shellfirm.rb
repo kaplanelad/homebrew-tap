467816b799070835a9a9bd3155c6cd2cbc452e45
Elad Kaplan <kaplan.elad@gmail.com>
2026-02-27 09:57:58 +0200

    updating formula for shellfirm

--- shellfirm.rb
+++ shellfirm.rb
@@ -2,9 +2,9 @@ class Shellfirm < Formula
   desc "Intercept any risky patterns (default or defined by you) and prompt you a small challenge for double verification"
   homepage "https://github.com/kaplanelad/shellfirm"
   license "Apache 2.0"
-  version "0.3.1"
-  url "https://github.com/kaplanelad/shellfirm/releases/download/v0.3.1/shellfirm-v0.3.1-x86_64-macos.tar.xz"
-  sha256 "2eb6054899571537dd4f8d10d2b1931d05cf52f27c7bd1a19ea40c719b97983e"
+  version "0.3.2"
+  url "https://github.com/kaplanelad/shellfirm/releases/download/v0.3.2/shellfirm-v0.3.2-x86_64-macos.tar.xz"
+  sha256 "290de7032bae19bd4f370eb405b6a6330b0aaae494a9b12487ceac007fe5d54a"
 
   def install
     bin.install "shellfirm"
